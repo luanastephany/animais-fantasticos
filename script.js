a153740223ae20c53a2b92e1bf92633d6cfefff0
Luana Stephany <ls.luanastephany@gmail.com>
2021-06-22 17:22:06 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -46,20 +46,36 @@ function initAccordion() {
 initAccordion()
 
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
-function scrollToSection(event) {
-  event.preventDefault()
-  const href = event.currentTarget.getAttribute('href')
-  const section = document.querySelector(href)
-  const topo = section.offsetTop
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    const topo = section.offsetTop
 
-  window.scrollTo({
-    top: topo,
-    behavior: 'smooth',
+    window.scrollTo({
+      top: topo,
+      behavior: 'smooth', //scroll suave
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+initScrollSuave()
+
+const sections = document.querySelectorAll('.js-scroll')
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top
+    if (sectionTop < 0) {
+      section.classList.add('ativo')
+    }
   })
 }
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSection)
-})
+window.addEventListener('scroll', animaScroll)
