282b35573e9f902966a38c56396b81880784431f
Luana Stephany <ls.luanastephany@gmail.com>
2021-06-22 16:35:48 -0300

    animacoes e scroll suave

--- script.js
+++ script.js
@@ -1,23 +1,65 @@
-//imagens
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-//descrições
-const tabContent = document.querySelectorAll('.js-tabcontent section')
+function initTabNav() {
+  //imagens
+  const tabMenu = document.querySelectorAll('.js-tabmenu li')
+  //descrições
+  const tabContent = document.querySelectorAll('.js-tabcontent section')
 
-if (tabMenu.length && tabContent.length) {
-  //sempre mostrar o 1º texto ao carregar a página
-  tabContent[0].classList.add('ativo')
+  if (tabMenu.length && tabContent.length) {
+    //sempre mostrar o 1º texto ao carregar a página
+    tabContent[0].classList.add('ativo')
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo')
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo')
+      })
+      tabContent[index].classList.add('ativo')
+    }
+
+    //loop pra cada item
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      })
     })
-    tabContent[index].classList.add('ativo')
   }
+}
+initTabNav()
 
-  //loop pra cada item
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index)
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  if (accordionList.length) {
+
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+
+    function activeAccordion() {
+      this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo')
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
     })
+  }
+}
+initAccordion()
+
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollToSection(event) {
+  event.preventDefault()
+  const href = event.currentTarget.getAttribute('href')
+  const section = document.querySelector(href)
+  const topo = section.offsetTop
+
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth',
   })
 }
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection)
+})
