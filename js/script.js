5a67d48ab0f1460e6326e14795bf973317d2348e
Luana Stephany <ls.luanastephany@gmail.com>
2021-06-27 17:13:57 -0300

    criando modules

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import anonima from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+
+initScrollSuave()
+anonima()
+initAccordion()
+initTabNav()
