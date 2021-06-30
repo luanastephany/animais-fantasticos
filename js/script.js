4e1547cc9a8396ec389b2e81a262c9a61af12d96
Luana Stephany <ls.luanastephany@gmail.com>
2021-06-30 20:57:05 -0300

    js do modal funcionando

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import animacao from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave()
 animacao()
 initAccordion()
 initTabNav()
+initModal()
