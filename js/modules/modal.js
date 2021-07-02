687f0379308c26378d85b7a754db322a15833681
Luana Stephany <ls.luanastephany@gmail.com>
2021-07-02 09:52:48 -0300

    mudando pra toggle

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,29 +1,23 @@
 export default function initModal() {
-
-}
-
-const botaoAbrir = document.querySelector('[data-modal="abrir"]')
-const botaoFechar = document.querySelector('[data-modal="fechar"]')
-const containerModal = document.querySelector('[data-modal="container"]')
+  const botaoAbrir = document.querySelector('[data-modal="abrir"]')
+  const botaoFechar = document.querySelector('[data-modal="fechar"]')
+  const containerModal = document.querySelector('[data-modal="container"]')
 
 
-function abrirModal(event) {
-  event.preventDefault()
-  containerModal.classList.add('ativo')
-}
-
-function fecharModal(event) {
-  event.preventDefault()
-  containerModal.classList.remove('ativo')
-}
-
-//fechar modal clicando do lado de fora dele
-function cliqueForaModal(event) {
-  if (event.target === this) {
-    fecharModal(event)
+  function toggleModal(event) {
+    event.preventDefault()
+    containerModal.classList.toggle('ativo')
   }
+
+  //fechar modal clicando do lado de fora dele
+  function cliqueForaModal(event) {
+    if (event.target === this) {
+      toggleModal(event)
+    }
+  }
+
+  botaoAbrir.addEventListener('click', toggleModal)
+  botaoFechar.addEventListener('click', toggleModal)
+  containerModal.addEventListener('click', cliqueForaModal)
 }
 
-botaoAbrir.addEventListener('click', abrirModal)
-botaoFechar.addEventListener('click', fecharModal)
-containerModal.addEventListener('click', cliqueForaModal)
