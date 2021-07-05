0e4164b8ea78ca9173c34c6bfb5d599d3df5cce6
Luana Stephany <ls.luanastephany@gmail.com>
2021-07-05 15:02:46 -0300

    usando fetch para api

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import animacao from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js"
 
 initScrollSuave()
 animacao()
 initAccordion()
 initTabNav()
 initModal()
+initFetchBitcoin()
